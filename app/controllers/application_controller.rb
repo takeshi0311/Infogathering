67901f69f3986b6356d47af38a7d87f41888e4dd
takeshi0311 <fabio.1993.2019@gmail.com>
2020-04-17 01:46:55 +0900

    capistranoの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :basic_auth, if: :production?
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     end
@@ -9,4 +9,16 @@ class ApplicationController < ActionController::Base
       # after_sign_out_path_for:deviseでサインアウトしたあとのリダイレクト先を指定するメソッド
       '/users/sign_in' # サインアウト後のリダイレクト先URL
     end
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
