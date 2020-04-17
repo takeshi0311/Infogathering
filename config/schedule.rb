8decfc3ce9da5bfffe7247fb1d181bc098034950
takeshi0311 <fabio.1993.2019@gmail.com>
2020-04-17 14:00:40 +0900

    バッチ処理修正５

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,7 +6,11 @@
 # Example:
 # set :environment, :development
 set :output, { :error => 'log/whenever.log', :standard => 'log/cron.log' }
-set :environment, :production
+
+# cronを実行する環境変数
+rails_env = ENV['RAILS_ENV'] || :development
+# cronを実行する環境変数をセット
+set :environment, rails_env
 
 every 3.minutes do
   runner "Scraping.saveinfo"
